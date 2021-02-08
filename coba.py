3fdeb3a0c269b48579fe73684727513798257b4f
R.Firman <raihan.firmansyah02@gmail.com>
2021-02-08 16:39:14 +0700

    second commit yeeee

--- coba.py
+++ coba.py
@@ -1,3 +1,14 @@
 print('='*20)
 print("Hello bitchhhhhhhy")
 print('='*20)
+print('='*20)
+print("Hello bitchhhhhhhy")
+print('='*20)
+print('='*20)
+print("Hello bitchhhhhhhy")
+print('='*20)
+print('='*20)
+print("Hello bitchhhhhhhy")
+print('='*20)
+
+
